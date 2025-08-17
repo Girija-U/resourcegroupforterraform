f5e903809ccfda3abd0b883e32cd988d4438a62f
Girija-U <girijaullagaddi505@gmail.com>
2025-08-17 16:26:44 +0530

    Create rg.tf

---
+++ rg.tf
@@ -0,0 +1,37 @@
+terraform { 
+  cloud { 
+    
+    organization = "girijau25" 
+
+    workspaces { 
+      name = "localfile" 
+    } 
+  } 
+}
+terraform {
+required_providers {
+azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+provider "azurerm" {
+features {}
+client_id="dd580511-ec92-4516-874e-b8a87100fb6c"
+client_secret="V.18Q~vq7fWIrARbKB3BTir1DQVrkKclIrMHyaTM"
+tenant_id="7597b8dc-1559-4285-901d-856b7ac20009"
+subscription_id="6babc3f8-793c-4200-9aa3-51e8d33ff572"
+}
+
+variable "rgname" {
+  default = "terraformrggu1"
+}
+variable "location" {
+  default = "West US 3"
+}
+resource "azurerm_resource_group" "example" {
+  name     = var.rgname
+  location = var.location
+}
